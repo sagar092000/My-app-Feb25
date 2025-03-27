cc89181f4a24c1af788388da7d5c6e8e7cd08d21
sagar092000 <sagarbabupullagura0@gmail.com>
2025-03-27 14:02:43 +0530

    implemented custom directives and custom pipes

---
+++ src/app/capital-directive.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appCapitalDirective]'
+})
+export class CapitalDirectiveDirective {
+
+  constructor(private _elementRef:ElementRef) { }
+
+  @HostListener('keyup')
+
+  change(){
+    this._elementRef.nativeElement.value= this._elementRef.nativeElement.value.toUpperCase()
+  }
+
+
+}
